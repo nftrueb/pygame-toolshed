1808ab775b646d8fd9362c8361d4798457da28fa
nftrueb <nftrueb@gmail.com>
2025-09-02 21:54:56 -0500

    created buttons for UI class

--- pause-menu-demo.py
+++ pause-menu-demo.py
@@ -5,6 +5,17 @@ from toolshed.ui import UI, Node
 
 WIDTH, HEIGHT = 320,180
 
+# atlas offsets 
+atlas_offsets = {
+	'A_INC'  : (53, 0, 10, 10),
+	'A_DEC'  : (43, 0, 10, 10),
+	'A_BACK' : (0, 0, 43, 19), 
+
+	'A_INC_INV' : (53, 10, 10, 10), 
+	'A_DEC_INV' : (43, 10, 10, 10), 
+	'A_BACK_INV': (0, 19, 43, 19), 
+}
+
 def get_window_scale(src, dest):
 	return min(dest[0] // src[0], dest[1] // src[1])
 
@@ -25,6 +36,16 @@ def main():
 	font_img = pg.image.load('assets/font.png').convert()
 	font_writer = FontSpriteWriter(font_img)
 
+	# load sprite assets 
+	atlas = pg.image.load('assets/toolshed-atlas.png').convert()
+	atlas.set_colorkey((255,0,255))
+	inc_img = atlas.subsurface(atlas_offsets['A_INC'])
+	inc_img_inv = atlas.subsurface(atlas_offsets['A_INC_INV'])
+	dec_img = atlas.subsurface(atlas_offsets['A_DEC'])
+	dec_img_inv = atlas.subsurface(atlas_offsets['A_DEC_INV'])
+	back_img = atlas.subsurface(atlas_offsets['A_BACK'])
+	back_img_inv = atlas.subsurface(atlas_offsets['A_BACK_INV'])
+
 	# creating UIs for pause menu and game menu 
 	button_w = WIDTH // 5 * 2
 	button_h = HEIGHT // 10
@@ -35,10 +56,31 @@ def main():
 	pause_ui.insert(Node('Quit', pg.Rect(96, 108, button_w, button_h)))
 
 	game_ui = UI()
-	game_ui.insert(Node('Pause', pg.Rect(5, 5, 40, 10)))
+	w, h = inc_img.get_size()
+	game_ui.insert(
+		Node('Pause', pg.Rect(5, 5, w, h), img=inc_img, pressed_img=inc_img_inv)
+	)
+
+	# slider and increments
+	options_ui = UI()
+	w, h = back_img.get_size()
+	options_ui.insert(
+		Node('Back', pg.Rect(139, 144, w, h), img=back_img, pressed_img=back_img_inv)
+	)
+	w, h = inc_img.get_size()
+	options_ui.insert(
+		Node('Inc', pg.Rect(260, 67, w, h), img=inc_img, pressed_img=inc_img_inv)
+	)
+	w, h = dec_img.get_size()
+	options_ui.insert(
+		Node('Dec', pg.Rect(211, 67, w, h), img=dec_img, pressed_img=dec_img_inv)
+	)
+
+	value = 0
 
 	running = True
 	paused = True 
+	options = False
 	while running:
 		# get mouse position at start of frame
 		mx, my = pg.mouse.get_pos()
@@ -46,7 +88,11 @@ def main():
 		my //= scale 
 
 		# set UI object based on current state
-		ui = pause_ui if paused else game_ui
+		ui = game_ui
+		if paused: 
+			ui = pause_ui
+		if options: 
+			ui = options_ui
 
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
@@ -69,6 +115,7 @@ def main():
 				node = ui.get_node((mx, my))
 				if node and isinstance(node, Node):
 					node.filled = False
+					# pause ui nodes
 					if node.tag == 'Resume': 
 						paused = False 
 
@@ -76,20 +123,28 @@ def main():
 						running = False 
 
 					if node.tag == 'Options': 
-						pass 
+						options = True 
 
+					# game ui nodes
 					if node.tag == 'Pause': 
 						paused = True
+
+					# options ui nodes
+					if node.tag == 'Back': 
+						options = False 
+
+					if node.tag == 'Dec': 
+						value = max(value-1, 0) 
+
+					if node.tag == 'Inc': 
+						value += 1 
 		
 		# clear new frame
 		frame.fill((255,255,255))
 		
-		# render UI 
-		if not paused: 
-			game_ui.draw(frame)
-		else: 
-			pause_ui.draw(frame)
+		ui.draw(frame)
 
+		if ui == pause_ui:
 			x = WIDTH//2
 			texts = {
 				'Resume': 45, 
@@ -100,6 +155,12 @@ def main():
 				text_w, text_h = font_writer.get_size(text)
 				dialogue = Dialogue(text, pg.Rect(x-text_w//2, y_pos-text_h//2, text_w, text_h))
 				font_writer.render(frame, dialogue)
+
+		if ui == options_ui: 
+			text = str(value)
+			w, h = font_writer.get_size(text)
+			dialogue = Dialogue(text, (241 - w//2, 68, w, h))
+			font_writer.render(frame, dialogue)
 
 		# scale the frame 
 		fw, fh = frame.get_size()
