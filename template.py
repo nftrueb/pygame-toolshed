988d7f5824b1e086f1a888ecd3fb1557025a2b6d
nftrueb <nftrueb@gmail.com>
2026-01-18 15:03:21 -0600

    Updated template with logger messages

--- template.py
+++ template.py
@@ -1,36 +1,37 @@
 import pygame as pg
 
-from toolshed.window import PygameContext, EventContext
+from toolshed.window import PygameContext
+from toolshed.logger import Logger
 
 WIDTH, HEIGHT = 320, 180
+
+log = Logger()
 
 class App: 
     def __init__(self): 
         icon = 'assets/icon.png'
         self.pc = PygameContext((WIDTH, HEIGHT), icon)
-        self.ec = EventContext((0,0))
         self.running = True
 
     def run(self): 
         try: 
             while self.running: 
-                self.ec = self.pc.get_event_context()
-                self.step_frame()
-
+                self.handle_event()
+                self.draw()
         except KeyboardInterrupt: 
-            pass 
+            log.info('KeyboardInterrupt recorded... exiting now') 
+        except Exception as ex: 
+            log.error(f'Error encounted in main game loop: {ex}') 
 
         pg.quit()
         print('Successfully exited program ...') 
 
-    def step_frame(self): 
-        self.handle_event()
-
+    def draw(self): 
         self.pc.frame.fill((255,255,255))
-
         self.pc.finish_drawing_frame()
 
     def handle_event(self): 
+        mx, my = self.pc.get_event_context().mouse_pos
         for event in pg.event.get(): 
             if event.type == pg.QUIT: 
                 self.running = False 
@@ -40,7 +41,7 @@ class App:
                     self.running = False 
 
             elif event.type == pg.MOUSEBUTTONUP: 
-                print(f'Mouse clicked at ({self.ec.mouse_pos})')
+                print(f'Mouse clicked at ({mx:.{2}f}, {my:.{2}f})')
 
 def main(): 
     app = App()
